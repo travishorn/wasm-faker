6387e9434a1c8b72a51e3309f87f2644ec7f5ee8
Travis Horn <travis@travishorn.com>
2023-03-26 13:11:00 -0500

    Random first, last, full names

---
+++ README.md
@@ -0,0 +1,17 @@
+# Faker
+
+Generate fake data for testing and development.
+
+Only first name, last name, and full name are supported right now. More data
+types are planned.
+
+First names are a combination of the top 50 male and top 50 female first names
+given to babies born in the USA during 1922-2021 according to the [Social
+Security
+Administration](https://www.ssa.gov/OACT/babynames/decades/century.html).
+
+Last names are the top 100 surnames in the USA according to the 2020 Census as
+reported by
+[ThoughCo.](https://www.thoughtco.com/most-common-us-surnames-1422656)
+
+Full names are a concatenation of first name and last name with a space.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,65 @@
+mod data {
+    pub mod first_names;
+    pub mod last_names;
+}
+
+use data::first_names::*;
+use data::last_names::*;
+use rand::prelude::*;
+
 wai_bindgen_rust::export!("faker.wai");
+
+fn get_random_string<'a>(strings: &'a [&'a str]) -> &'a str {
+    let mut rng = thread_rng();
+    strings.choose(&mut rng).unwrap()
+}
 
 pub struct Faker;
 
 impl faker::Faker for Faker {
+    fn first_name() -> String {
+        String::from(get_random_string(&FIRST_NAMES))
+    }
+
+    fn last_name() -> String {
+        String::from(get_random_string(&LAST_NAMES))
+    }
+
     fn full_name() -> String {
-        String::from("John Doe")
+        let first_name = Self::first_name();
+        let last_name = Self::last_name();
+        format!("{} {}", first_name, last_name)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use faker::Faker as _;
+
+    #[test]
+    fn first_name() {
+        let result = Faker::first_name();
+        assert!(FIRST_NAMES.contains(&result.as_str()));
+    }
+
+    #[test]
+    fn last_name() {
+        let result = Faker::last_name();
+        assert!(LAST_NAMES.contains(&result.as_str()));
+    }
+
+    #[test]
+    fn full_name() {
+        let result = Faker::full_name();
+        let split_result: Vec<&str> = result.split_whitespace().collect();
+
+        assert_eq!(split_result.len(), 2);
+
+        let first_name = split_result[0];
+        let last_name = split_result[1];
+
+        assert!(FIRST_NAMES.contains(&first_name));
+        assert!(LAST_NAMES.contains(&last_name));
     }
 }
